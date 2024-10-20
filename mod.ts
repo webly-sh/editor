c370992794d2aabb660ceda053ee81ff32323ee1
kev-techi <kevin@techi.be>
2024-10-20 12:18:54 +0200

    use a ts module

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export const weblyModule = {
+  name: "Editor",
+  description: "Simple page editor for webly",
+  type: "spa",
+  dist: "./dist.tar.gz",
+  repository: "https://github.com/webly-sh/editor",
+};
