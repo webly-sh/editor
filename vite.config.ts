45291576d904611c09b5b0dfae93b9087127d332
kev-techi <kevin@techi.be>
2024-10-20 18:17:35 +0200

    compile under proper path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Deno.env.get("BASE_URL") || "/admin/editor",
+  base: Deno.env.get("BASE_URL") || "/admin/pages/editor",
 });
